b2a3fc17b4b3830eb2333b6b0e1da1f193ddeab1
MariiaSam <kontragent@ukr.net>
2023-11-01 12:34:40 +0200

    done

--- weather.js
+++ weather.js
@@ -1,46 +1,59 @@
 // KEY 21 DAYS from 31.10.2023
 // 18979647cea74422aec120005233110
 
-
-const elem = {
-    form: document.querySelector('.js-search-form'),
-    list: document.querySelector('.js-list'),
-}
-
-console.log(elem.form) 
-
-
-elem.form.addEventListener('submit', handlerForecast )
-
-function handlerForecast(e){
-    e.preventDefault()
-    console.dir(e.currentTarget)
-
-    const { city, days} = e.currentTarget.elements
-
-    console.log()
-    console.log
-
-    servisWeather(city.value, days.value)
-.then(data => console.log(data))
-.catch(err => console.log(err))
-
-
-}
-
-
-
-function servisWeather(city, days){
-const BASE_URL = 'http://api.weatherapi.com/v1';
-const API_KEY = '18979647cea74422aec120005233110';
-
-return fetch(`${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&${days}=5&lang=uk`)
-.then(resp => {
-if(!resp.ok){
- throw new Error(resp.statusText)
-}
-return resp.json
-})
-}
-
-
+const elements = {
+    form: document.querySelector(".js-search-form"),
+    list: document.querySelector(".js-list"),
+  };
+  
+  elements.form.addEventListener("submit", handlerForecast);
+  
+  function handlerForecast(evt) {
+    evt.preventDefault();
+  
+    const { city, days } = evt.currentTarget.elements;
+  
+    serviceWeather(city.value, days.value)
+      .then((data) => elements.list.innerHTML = createMarkup(data.forecast.forecastday))
+      .catch((err) => console.log(err));
+  }
+  
+  function serviceWeather(city, days) {
+    const BASE_URL = "http://api.weatherapi.com/v1";
+    const API_KEY = "18979647cea74422aec120005233110";
+    const params = new URLSearchParams({
+      key: API_KEY,
+      q: city,
+      lang: "uk",
+      days,
+    });
+  
+    return fetch(`${BASE_URL}/forecast.json?${params}`).then((resp) => {
+      console.log(resp);
+      if (!resp.ok) {
+        throw new Error(resp.statusText);
+      }
+  
+      return resp.json();
+    });
+  }
+  
+  function createMarkup(arr) {
+    return arr
+      .map(
+        ({
+          date,
+          day: {
+            avgtemp_c,
+            condition: { text, icon },
+          },
+        }) => `
+      <li class="weather-card">
+          <img src="${icon}" alt="${text}" class="weather-icon" />
+          <h2 class="date">${date}</h2>
+          <h3 class="weather-text">${text}</h3>
+          <h3 class="temperature">${avgtemp_c} °C</h3>
+      </li>`
+      )
+      .join("");
+  }
